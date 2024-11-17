c3520efb0d5dba58204b22740b8994bb8719e7ce
Mohammed Siddiq <ms2325558@gmail.com>
2024-11-17 17:15:35 +0000

    final version of my website (or maybe not) haha

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.style.opacity = '1';
             entry.target.style.transform = 'translateY(0)';
+
+            document.querySelector('.download-btn').addEventListener('mousemove', function(e) {
+                const btn = this;
+                const rect = btn.getBoundingClientRect();
+                const x = e.clientX - rect.left;
+                const y = e.clientY - rect.top;
+                
+                const centerX = rect.width / 2;
+                const centerY = rect.height / 2;
+                
+                const deltaX = (x - centerX) / 8;
+                const deltaY = (y - centerY) / 8;
+                
+                btn.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+            });
+            
+            document.querySelector('.download-btn').addEventListener('mouseleave', function() {
+                this.style.transform = 'translate(0, 0)';
+            });
